eb69a1ffe73907e7ce3c0f27f49631399fabf3e9
Felipe Tavares <xfelipetavares@gmail.com>
2021-05-31 16:55:22 -0300

    começando meu projeto da calculadora em C.

--- Calculadora/main.c
+++ Calculadora/main.c
@@ -1,15 +1,11 @@
-//
-//  main.c
-//  Calculadora
-//
-//  Created by Felipe Tavares on 31/05/21.
-//
-
 #include <stdio.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
-    return 0;
+int num1, num2, result;
+char continuar = 's';
+
+int main (void){
     
+    
+    
+    return 0;
 }
