dd8157c9e396cbbdbc023ab2fd340015db198519
Felipe Tavares <xfelipetavares@gmail.com>
2021-05-31 15:58:32 -0300

    Initial Commit

---
+++ Calculadora/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Calculadora
+//
+//  Created by Felipe Tavares on 31/05/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
