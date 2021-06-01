8aa2f822ab58faef800ccbf0d9f07324e99d5c54
Felipe Tavares <xfelipetavares@gmail.com>
2021-06-01 17:38:40 -0300

    do while na calc!
    adicionei um wo while para a calculadora so parar quando o usuario decidir

--- Calculadora/calculadora.c
+++ Calculadora/calculadora.c
@@ -9,40 +9,42 @@ int operador = 0;
 float result, num1, num2;
 
 int main (void){
+
+    do {
+        printf("Digite um número: ");
+        scanf("%f", &num1);
+        printf("Digite outro número: ");
+        scanf("%f", &num2);
         
-    printf("Digite um número: ");
-    scanf("%f", &num1);
-    printf("Digite outro número: ");
-    scanf("%f", &num2);
-    
-    printf("\nDigite 1 para + (Somar os numeros digitados)\n");
-    printf("Digite 2 para - (Subtrair os numeros digitados)\n");
-    printf("Digite 3 para * (Multiplicar os numeros digitados)\n");
-    printf("Digite 4 para / (Dividir os numeros digitados)\n");
-    printf("\nEu escolho... ");
-    scanf("%i", &operador);
+        printf("\nDigite 1 para + (Somar os numeros digitados)\n");
+        printf("Digite 2 para - (Subtrair os numeros digitados)\n");
+        printf("Digite 3 para * (Multiplicar os numeros digitados)\n");
+        printf("Digite 4 para / (Dividir os numeros digitados)\n");
+        printf("Digite 0 para / (Sair)\n");
+        printf("\nEu escolho... ");
+        scanf("%i", &operador);
+            
+            switch (operador) {
+                case 1:
+                    result = num1 + num2;
+                    break;
+                case 2:
+                    result = num1 - num2;
+                    break;
+                case 3:
+                    result = num1 * num2;
+                    break;
+                case 4:
+                    result = num1 / num2;
+                    break;
+                    
+                default: printf("Até a Proxima!");
+                    break;
+            }
         
-        switch (operador) {
-            case 1:
-                result = num1 + num2;
-                break;
-            case 2:
-                result = num1 - num2;
-                break;
-            case 3:
-                result = num1 * num2;
-                break;
-            case 4:
-                result = num1 / num2;
-                break;
-                
-            default: printf("voce nao escolheu um numero valido");
-                break;
-        }
-    
-    printf("\nO resultado é: %.2f\n\n", result);
-    
-    
+        printf("\nO resultado é: %.2f\n\n", result);
+
+    } while (operador != 0);
     
     return 0;
 }
